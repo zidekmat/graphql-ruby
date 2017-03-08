54f5a13efe4df1050865647bdd54eabfa9bcf30d
Robert Mosolgo <rdmosolgo@gmail.com>
2017-03-08 17:37:50 -0500

    Add test for GraphQL::Client

--- spec/graphql/schema/loader_spec.rb
+++ spec/graphql/schema/loader_spec.rb
@@ -2,67 +2,6 @@
 require "spec_helper"
 
 describe GraphQL::Schema::Loader do
-  describe "bug" do
-    let(:defn_json) { JSON.parse(File.read("./spec/support/bug_schema.json")) }
-    let(:schema) { GraphQL::Schema.from_introspection(defn_json) }
-
-    module RootObj
-      module_function
-
-      def locationAll
-        [
-          OpenStruct.new({
-            id: 1,
-            name: "Cool Place",
-            address: "123 Easy St",
-            city: "Anytown",
-            state: "CA",
-            zip: "92008",
-            phoneNumber: "555-1234",
-            latitude: 37.5,
-            longitude: 100.0,
-          })
-        ]
-      end
-    end
-
-    it "executes" do
-      query_str = <<-GRAPHQL
-        query {
-          locationAll {
-            ...LocationFragment
-          }
-        }
-        fragment LocationFragment on Location {
-          id
-          name
-          address
-          city
-          state
-          zip
-          phoneNumber
-          latitude
-          longitude
-        }
-      GRAPHQL
-
-      res = schema.execute(query_str, root_value: RootObj)
-      expected_result = {
-        "id" => "1",
-        "name" => "Cool Place",
-        "address" => "123 Easy St",
-        "city" => "Anytown",
-        "state" => "CA",
-        "zip" => "92008",
-        "phoneNumber" => "555-1234",
-        "latitude" => "37.5",
-        "longitude" => "100.0",
-      }
-
-      assert_equal expected_result, res["data"]["locationAll"][0]
-    end
-  end
-
   let(:schema) {
     node_type = GraphQL::InterfaceType.define do
       name "Node"
